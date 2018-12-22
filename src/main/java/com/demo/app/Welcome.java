41664f7fe364eb3c4b1c050f23460292f0818980
abdellah <bellahcene.abdellah@gmail.com>
2018-12-22 17:06:02 +0100

    rederect response to aws LB

--- src/main/java/com/demo/app/Welcome.java
+++ src/main/java/com/demo/app/Welcome.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class Welcome {    
     @RequestMapping("/")
     public String index() {
-        return "Salut les Gens !!";
+        return "<h2>Hey Softeamiens !!</h2>";
     }
 
 }
--- src/main/java/com/demo/app/WelcomeControllerTest.java
+++ src/main/java/com/demo/app/WelcomeControllerTest.java
@@ -25,7 +25,7 @@ public class WelcomeControllerTest {
     public void getHello() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("Greetings from Spring Boot!")));
+                .andExpect(content().string(equalTo("<h2>Hey Softeamiens !!</h2>")));
     }
 
 }
