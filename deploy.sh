d2b54a3d5425d0f42e384e75900542a54d83730a
abdellah <bellahcene.abdellah@gmail.com>
2019-01-12 17:23:32 +0100

    add script to deploy serivce on aws

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+REGION=eu-west-3
+CLUSTER=rater-cluster
+ls
+FAMILY=$(sed -n 's/.*"family": "\(.*\)",/\1/p' rater-task-definition.json)
+echo "family is $FAMILY"
+NAME=$(sed -n 's/.*"name": "\(.*\)",/\1/p' rater-task-definition.json)
+SERVICE_NAME=${NAME}-service
+echo "service name is $SERVICE_NAME"
+
+BUILD_NUMBER=2
+
+aws --profile abdel ecs register-task-definition --cli-input-json file://rater-task-definition.json
+SERVICES=$(aws --profile abdel ecs describe-services --services ${SERVICE_NAME} --cluster ${CLUSTER} | jq .failures[])
+echo "SERVICES is $SERVICES"
+#Get latest revision
+REVISION=$(aws --profile abdel ecs describe-task-definition --task-definition ${FAMILY} | jq .taskDefinition.revision)
+echo "REVISION is $REVISION"
+
+#Create or update service
+if [ "$SERVICES" == "" ]; then
+  echo "entered existing service"
+  aws --profile abdel ecs update-service --cluster ${CLUSTER} --region ${REGION} --service ${SERVICE_NAME} --task-definition ${FAMILY}:${REVISION} --desired-count 1
+else
+  echo "entered new service"
+  aws --profile abdel ecs create-service --task-definition ${FAMILY} --cli-input-json file://rater-ecs-service.json
+fi
